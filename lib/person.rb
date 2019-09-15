e1c2cae2a70871067ddfdca791a5cd982564ef8d
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-09-15 17:56:24 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+end
