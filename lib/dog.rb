867cee06b377e41a6900628084e1ce52b8f795e0
Jalysa Reason <jalysareason@gmail.com>
2019-09-25 10:46:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/dog'
+
+def class(Dog)
+ def initialize(name)
+   @name = name
+ end
+ 
+ Dog = Dog.new("Rocket")
+ Dog.name
+ 
+ 
+def class(Dog)
+ def initialize(breed)
+   #breed = breed
+ end
+ 
+ Dog = Dog.new("Pitbull")
+ Dog.breed
+  
