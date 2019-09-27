1450ac488af81bde68d720dd899065a3f1b79a8b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-27 13:51:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+<<<<<<< HEAD
   def name=(new_name)
     @name = new_name
   end
@@ -14,3 +15,16 @@ class Dog
   end
 end
 
+=======
+  def initialize(name)
+    @name = name
+  end
+end
+
+# 
+ #   @name = name
+#  end
+#  def name
+#    @name
+ # end
+>>>>>>> 845151c3d26b66957135e403320d678aae4d5052
