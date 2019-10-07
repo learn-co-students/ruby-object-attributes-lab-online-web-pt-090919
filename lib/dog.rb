d78de454d06c4cc1d3a5ec19416ce53313ff0612
thomas nguyen <tommi.t.nguyen@icloud.com>
2019-10-07 07:16:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+ 
+  def Dog_with_names(name)
+    @name =name
+  end
+ 
+  def name
+    @name
+  end
+   
+   def name=(new_name)
+    @name = new_name
+  end
+
+  def Dog_with_breeds(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+ 
+  def Person_with_names(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  def Person_with_jobs(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end 
+end
