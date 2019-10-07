68c6c1a6dbb592df990e89d38704f01df258ef09
thomas nguyen <tommi.t.nguyen@icloud.com>
2019-10-07 07:43:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,19 @@
 class Dog
  
-  def Dog_with_names(name)
-    @name =name
+  def name=(name)
+    @name = name
   end
- 
+  
   def name
     @name
   end
-   
-   def name=(new_name)
-    @name = new_name
-  end
+  
 
-  def Dog_with_breeds(breed)
+ def breed=(breed)
     @breed = breed
   end
-  
+ 
   def breed
     @breed
-  end
-  
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end 
+end
 end
