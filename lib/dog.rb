663891ce70fdca4144184d22255eb5e0d1022982
Joshua Rodriguez <josh030596@gmail.com>
2019-10-23 00:56:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def initialze(name)
+    @name = name 
+  end
+  def name 
+    @name 
+  end 
+  def name=(new_name)
+    @name = new_name
+  end 
+  def intialze(breed)
+    @breed = breed 
+  end 
+  def breed 
+    @breed 
+  end 
+  def breed=(new_breed)
+    @breed = new_breed 
+  end 
+end 
